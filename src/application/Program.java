12d1c2662c3425bf7cb2d44270c74e2e414fbca6
maurisudario <maurisudario@icloud.com>
2020-09-29 16:32:28 -0300

    Nineth class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import chess.ChassMatch;
+import chess.ChessException;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
@@ -16,17 +18,30 @@ public class Program {
 		
 		
 		while (true) {
-		UI.printBoard(chessMatch.getPieces());
-		System.out.println();
-		System.out.print("Source: ");
-		ChessPosition source = UI.readChessPosition(sc);
+			try {
+				UI.clearScreen();	
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				}
 		
-		System.out.println();
-		System.out.print("Target: ");
-		ChessPosition target = UI.readChessPosition(sc);
-		
-		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.hasNextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.hasNextLine();
+			
 		}
 	}
-
+	}
 }
+
